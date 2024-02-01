e6a7a53756871b4d61b29bdb86b6ca9f18831cdd
RareSightGames <saitom@raresightgames.com>
2024-02-01 15:54:24 +0900

    Button のプレハブ化 ButtonController を Button1 のプレハブにアタッチ

--- Assets/ButtonController.cs
+++ Assets/ButtonController.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class ButtonController : MonoBehaviour
 {
     [SerializeField] private GameObject Button_0;
+    [SerializeField] private int ButtonIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        TMPro.TextMeshProUGUI buttonText = this.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        buttonText.text = ButtonIndex.ToString();
     }
 
     // Update is called once per frame
@@ -23,11 +25,11 @@ public class ButtonController : MonoBehaviour
     // ボタンが押されたらButton0の座標と入れ替える
     public void ChangeButtonPositionToZero() {
         // 押されたボタンを取得
-        Button clickedButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+//        Button clickedButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
 
         // ボタン0と座標を入れ替える
-        Vector3 tmpPosition = clickedButton.transform.position;
-        clickedButton.transform.position = Button_0.transform.position;
+        Vector3 tmpPosition = this.transform.position;
+        this.transform.position = Button_0.transform.position;
         Button_0.transform.position = tmpPosition;
     }
 }
